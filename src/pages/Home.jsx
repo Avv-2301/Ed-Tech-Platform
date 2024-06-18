846b6d61c9722119db0c9b6aca550cf6f0dbd94a
Akshat <akshatvijayvergiya64@gmail.com>
2024-06-18 18:24:03 +0530

    Third Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa6";
 import HighlightText from "../components/core/HomePage/HighlightText";
 import CTAButton from "../components/core/HomePage/Button";
-import Banner from '../assets/Images/banner.mp4';
+import Banner from "../assets/Images/banner.mp4";
 import CodeBlocks from "../components/core/HomePage/CodeBlocks";
 
 const Home = () => {
@@ -43,52 +43,47 @@ const Home = () => {
         </div>
 
         <div className=" mx-3 my-12 shadow-blue-200 shadow-[10px_-5px_50px_-5px]">
-          <video className="shadow-[20px_20px_rgba(255,255,255)]"
-          muted 
-          loop
-          autoPlay>
-            <source src={Banner} type="video/mp4"/>
+          <video
+            className="shadow-[20px_20px_rgba(255,255,255)]"
+            muted
+            loop
+            autoPlay
+          >
+            <source src={Banner} type="video/mp4" />
           </video>
         </div>
 
         {/* Code Section 1 */}
         <div>
-        <CodeBlocks
+          <CodeBlocks
             position={"lg:flex-row "}
             heading={
-                <div className="text-4xl font-semibold">
-                    Unlock Your <HighlightText text={"coding potential"}/>
-                    with our online courses
-                </div>
+              <div className="text-4xl font-semibold">
+                Unlock Your <HighlightText text={"coding potential"} />
+                with our online courses
+              </div>
             }
-
             subheading={
-                "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
+              "Our courses are designed and taught by industry experts who have years of experience in coding and are passionate about sharing their knowledge with you."
             }
-            ctabtn1={
-                {
-                    btnText:"Try it yourself",
-                    linkto:"/signup",
-                    active:true,
-                }
-            }
-
-            ctabtn2={
-                {
-                    btnText:"Learn more",
-                    linkto:"/login",
-                    active:false,
-                }
-            }
-
+            ctabtn1={{
+              btnText: "Try it yourself",
+              linkto: "/signup",
+              active: true,
+            }}
+            ctabtn2={{
+              btnText: "Learn more",
+              linkto: "/login",
+              active: false,
+            }}
             codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
             codeColor={"text-yellow-25"}
             backgroundGradient={<div className="codeblock1 absolute"></div>}
-        />
+          />
         </div>
 
-         {/* Code Section 2 */}
-         <div>
+        {/* Code Section 2 */}
+        <div>
           <CodeBlocks
             position={"lg:flex-row-reverse"}
             heading={
@@ -118,10 +113,29 @@ const Home = () => {
       </div>
 
       {/* Section 2 */}
+      <div className="bg-pure-greys-5 text-richblack-700">
+        <div className="homepage_bg h-[320px]">
+          {/* Explore Full Catagory Section */}
+          <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
+            <div className="lg:h-[150px]"></div>
+            <div className="flex flex-row gap-7 text-white lg:mt-8">
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="flex items-center gap-2">
+                  Explore Full Catalog
+                  <FaArrowRight />
+                </div>
+              </CTAButton>
+              <CTAButton active={false} linkto={"/login"}>
+                Learn More
+              </CTAButton>
+            </div>
+          </div>
+        </div>
 
-      {/* Section 3 */}
+        {/* Section 3 */}
 
-      {/* Footer */}
+        {/* Footer */}
+      </div>
     </div>
   );
 };
